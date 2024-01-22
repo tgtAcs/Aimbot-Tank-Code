62fa679db2584447396339eb30635e072d4faaf8
tgtAcs <145060549+tgtAcs@users.noreply.github.com>
2024-01-22 18:51:07 -0500

    Update Color_Detection.py

--- Color_Detection.py
+++ Color_Detection.py
@@ -1,10 +1,29 @@
+import numpy as np
 import cv2
 from PIL import Image
-from util import get_limits
 import serial
 import time
 import subprocess
 import sys
+
+def get_limits(color):
+    c = np.uint8([[color]])  # BGR values
+    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+
+    hue = hsvC[0][0][0]  # Get the hue value
+
+    # Handle red hue wrap-around
+    if hue >= 165:  # Upper limit for divided red hue
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([180, 255, 255], dtype=np.uint8)
+    elif hue <= 15:  # Lower limit for divided red hue
+        lowerLimit = np.array([0, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+    else:
+        lowerLimit = np.array([hue - 10, 100, 100], dtype=np.uint8)
+        upperLimit = np.array([hue + 10, 255, 255], dtype=np.uint8)
+
+    return lowerLimit, upperLimit
 
 try:
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
@@ -88,3 +107,4 @@ finally:
     cap.release()
     cv2.destroyAllWindows()
     ser.close()  # Close the serial port
+
